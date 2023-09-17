f1727780a2ff959f9e6d7d48223f59f735a2dffd
Aiida7923 <s2000ap104067@gmail.com>
2023-09-18 08:28:24 +0900

    Java課題を38~41を実施

---
+++ Sample/src/javatask/Task40.java
@@ -0,0 +1,29 @@
+public class Task40 {
+    void method(String arg1, int arg2) {
+
+        for (int i = 0; i < 10; i++) {
+            String str2 = "de";
+
+            if (i < 3) {
+                System.out.println(str2);
+            } else if (i < 7) {
+                System.out.println(arg2);
+            } else {
+                String str3 = "fghij";
+                System.out.println(str3);
+                System.out.println(str2 + str3);
+            }
+        }
+
+        if (arg2 > 0) {
+            String str1 = "abc";
+            String str4 = "klm";
+            System.out.println(str1 + str4);
+
+            int num = 10;
+            if (num < 20) {
+                System.out.println(str1);
+            }
+        }
+    }
+}
---
+++ Sample/src/javatask/Trapezoid.java
@@ -0,0 +1,18 @@
+public class Trapezoid {
+
+    public double topSide;
+    public double bottomSide;
+    public double height;
+
+    public Trapezoid(double topSide, double bottomSide, double height) {
+        this.topSide = topSide;
+        this.bottomSide = bottomSide;
+        this.height = height;
+        System.out.println("上底が" + topSide + "、下底が" + bottomSide + "、高さが" + height + "の台形を作成しました。");
+    }
+
+    public double calculateArea() {
+        return 0.5 * (topSide + bottomSide) * height;
+    }
+
+}
